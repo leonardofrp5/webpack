105290d3c6d5974df3ee29e4ef8fc1c53596d4ae
leonardofrp5 <leonardofrp5@gmail.com>
2021-07-25 20:25:28 -0500

    added some plugins for loader of img

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
+import styles from './styles.css';
+import Logo from './assets/webpack.svg';
+import geminis from './assets/saga-kanon.png';
+
 const array = [1, 2, 3];
 
-codeESNext = () => console.log(...array);
+const codeESNext = () => console.log(...array);
 
-console.log('hello');
+console.log('Hello');
 
 codeESNext();
+
+// document.getElementById('app').innerHTML = `<img src="${Logo}" alt="logo" />`;
+
+const d = document,
+  $app = d.getElementById('app'),
+  $h1 = d.createElement('h1'),
+  $logo = d.createElement('img'),
+  $img = d.createElement('img');
+
+$h1.textContent = 'Probando vista';
+$logo.src = Logo;
+$logo.classList.add('icon');
+$img.src = geminis;
+
+$app.appendChild($h1);
+$app.appendChild($logo);
+$app.appendChild($img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPligin = require('html-webpack-plugin');
+const { loader } = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   module: {
@@ -20,6 +22,27 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.css$/i,
+        /* use: [MiniCssExtractPlugin.loader, "css-loader"], */
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './'
+            }
+          },
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|webp)$/i,
+        use: 'file-loader?name=assets/[name].[ext]'
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])$/i,
+        use: 'file-loader?name=./fonts/[name].[ext]'
       }
     ]
   },
@@ -27,6 +50,7 @@ module.exports = {
     new HtmlWebpackPligin({
       template: './src/index.html',
       filename: './index.html'
-    })
+    }),
+    new MiniCssExtractPlugin()
   ]
 };
